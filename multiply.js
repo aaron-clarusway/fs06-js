f356f602407b89136c98977ccc919f67035a9547
aaron-clarusway <aaron.clarusway@gmail.com>
2020-09-22 23:06:26 +0300

    updated assgnment

--- multiply.js
+++ multiply.js
@@ -1,6 +1,6 @@
 const add = // Write Code Here
-const mul = // Write Code Here
-const sub = // Write Code Here
-const div = // Write Code Here
+    const mul = // Write Code Here
+        const sub = // Write Code Here
+            const div = // Write Code Here
 
-module.exports = { add, mul, sub, div };
+                module.exports = { add, mul, sub, div };
